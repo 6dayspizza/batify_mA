3e5ae4c20ced8e8ab2e8b60f677b41b8067c8be2
yesimverytired <kistlerr@oregonstate.edu>
2024-05-27 11:54:02 +0200

    ran Prettier, removed redundant code

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # INSTRUCTIONS TO IMPLEMENT MICROSERVICE A
+
 for Node.js/Express, and assuming these are already installed. if not using Node.js/Express, adapt instructions to language.\
 
 ## setting up sender:
+
 1. app reads a CSV with golf quotes. find it [here on Github](https://github.com/6dayspizza/batify_mA/blob/main/files/quotes_golf.csv).
 2. randomly selects one
 3. packs it into a JSON object and sends into the void through HTTP request. example:\
    `{"id":"8","quote":"I know I am getting better at golf because I am hitting fewer spectators.","author":"Gerald Ford","tags":"golf"}`
 
 ## setting up receiver:
+
 1. in _app.js_ of main app, write:\
    `const axios = require('axios');`
 2. install new dependency:\
    `npm install axios`
 3. set up axios call, in this example, I wanted the data in the header of my main app and wrote this in my _main.js_ file, adapt to yours:
+
    ```
    async function fetchData() {
        try {
@@ -24,11 +28,11 @@ for Node.js/Express, and assuming these are already installed. if not using Node
            console.error('Error fetching data from microservice:', error);
        }
    }
-   
+
    async function showQuote() {
        const data = await fetchData();
        const { quote, author } = data;
-   
+
        const quoteAuthor = document.getElementById('quoteAuthor');
        quoteAuthor.innerHTML = `<p>"${quote}"</p><p> (${author})</p>`;
        quoteAuthor.classList.toggle('show');
@@ -36,8 +40,8 @@ for Node.js/Express, and assuming these are already installed. if not using Node
    ```
 
 4. the above call the JSON object. on the header.hbs page, I then called the _showQuote()_-function:\
-   `<button id="popupTrigger" class="popup" onclick="showQuote()">`\
-\
-and that was it!
+    `<button id="popupTrigger" class="popup" onclick="showQuote()">`\
+   \
+   and that was it!
 
 ![UML](uml_diagram.png)
--- app.js
+++ app.js
@@ -2,19 +2,17 @@
     IMPORTS
 */
 
-const express = require('express');
-const { engine } = require("express-handlebars");
-const exphbs = require("express-handlebars");
-const fs = require('fs');
-const csv = require('csv-parser');
-const cors = require('cors');
+const express = require("express");
+const fs = require("fs");
+const csv = require("csv-parser");
+const cors = require("cors");
 const corsOptions = {
-    origin: [
-        'https://lit-everglades-39146-fd2b4b5a3c5f.herokuapp.com',
-        'http://localhost:3000',
-        'https://caddybuddy-1b6344ebc937.herokuapp.com/',
-      ],
-    optionsSuccessStatus: 200,
+  origin: [
+    "https://lit-everglades-39146-fd2b4b5a3c5f.herokuapp.com",
+    "http://localhost:3000",
+    "https://caddybuddy-1b6344ebc937.herokuapp.com/",
+  ],
+  optionsSuccessStatus: 200,
 };
 
 /*
@@ -29,68 +27,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("views/public"));
 app.use(cors(corsOptions));
 
-const hbs = exphbs.create({
-  partialsDir: "views/partials",
-  extname: ".hbs",
-});
-app.engine(".hbs", hbs.engine);
-app.set("view engine", ".hbs");
+/*
+    FILE READING
+*/
 
-app.get('/', (req, res) => {
-    res.send('hi microserviceA');
-});
+const filePathGolf = "files/quotes_golf.csv";
+const filePathBats = "files/quotes_bats.csv";
 
-const filePathGolf = 'files/quotes_golf.csv';
-const filePathBats = 'files/quotes_bats.csv';
-
-const readCSVFileGolf = (filePathGolf) => {
-    return new Promise((resolve, reject) => {
-        const lines = [];
-        fs.createReadStream(filePathGolf)
-            .pipe(csv())
-            .on('data', (data) => lines.push(data))
-            .on('end', () => resolve(lines))
-            .on('error', (error) => reject(error));
-    });
+const readCSVFile = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const lines = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => lines.push(data))
+      .on("end", () => resolve(lines))
+      .on("error", (error) => reject(error));
+  });
 };
 
-app.get('/random-quote-golf', async (req, res) => {
+const handleRandomQuoteRoute = (filePath) => {
+  return async (req, res) => {
     try {
-        const lines = await readCSVFileGolf(filePathGolf);
-        const randomIndex = Math.floor(Math.random() * lines.length);
-        const randomQuote = lines[randomIndex];
-        res.json(randomQuote);
+      const lines = await readCSVFile(filePath);
+      const randomIndex = Math.floor(Math.random() * lines.length);
+      const randomQuote = lines[randomIndex];
+      res.json(randomQuote);
     } catch (error) {
-        console.error('Error reading CSV file:', error);
-        res.status(500).send('Internal Server Error');
+      console.error("Error reading CSV file:", error);
+      res.status(500).send("Internal Server Error");
     }
-});
-
-const readCSVFileBats = (filePathBats) => {
-    return new Promise((resolve, reject) => {
-        const lines = [];
-        fs.createReadStream(filePathBats)
-            .pipe(csv())
-            .on('data', (data) => lines.push(data))
-            .on('end', () => resolve(lines))
-            .on('error', (error) => reject(error));
-    });
+  };
 };
 
-app.get('/random-quote-bats', async (req, res) => {
-    try {
-        const lines = await readCSVFileBats(filePathBats);
-        const randomIndex = Math.floor(Math.random() * lines.length);
-        const randomQuote = lines[randomIndex];
-        res.json(randomQuote);
-    } catch (error) {
-        console.error('Error reading CSV file:', error);
-        res.status(500).send('Internal Server Error');
-    }
+/*
+    GET REQUESTS
+*/
+
+app.get("/random-quote-golf", handleRandomQuoteRoute(filePathGolf));
+app.get("/random-quote-bats", handleRandomQuoteRoute(filePathBats));
+app.get("/", (req, res) => {
+  res.send("hi microserviceA");
 });
-
-
 
 app.listen(port, () => {
-    console.log(`Microservice listening at http://localhost:${port}`);
+  console.log(`Microservice listening at http://localhost:${port}`);
 });
